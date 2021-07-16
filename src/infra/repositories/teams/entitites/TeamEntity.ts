154082d13a2810d458853386c322a8593111118a
phtre57 <phtre57@gmail.com>
2021-07-16 15:28:15 -0400

    team entity trolls

--- src/infra/repositories/teams/entitites/TeamEntity.ts
+++ src/infra/repositories/teams/entitites/TeamEntity.ts
@@ -1,7 +1,18 @@
 import { Racer } from 'domain/racers/Racer';
+import { Team } from 'domain/teams/Team';
+import { TeamId } from 'domain/teams/TeamId';
+import { Document } from 'mongodb';
 
 export interface TeamEntity {
   id: string;
   name: string;
   racers: Record<string, Racer>;
 }
+
+export const buildTeam = (document: Document) => {
+  return new Team({
+    id: new TeamId(document.id),
+    name: document.name,
+    racers: document.racers,
+  });
+};
